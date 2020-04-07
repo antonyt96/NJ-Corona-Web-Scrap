19bc0236d43583dc688cd264cad143346f603ed8
antonyt96 <anthonytejad21@gmail.com>
2020-04-07 19:19:42 -0400

    test timer

--- index.js
+++ index.js
@@ -15,36 +15,36 @@ app.set("view engine", "ejs");
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+schedule.scheduleJob("22 19 * * 0-6", function(){
+
+	axios.get('https://www.worldometers.info/coronavirus/country/us/').then((res)=>{
+
+	const $ = cheerio.load(res.data);
+
+	const njTotalCases = $("#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(2)").text();
+	const njNewCases = $("#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(3)").text();
+	const njTotalDeaths =$("#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(4)").text();
+	const njNewDeaths = $("#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(5)").text();
+	const njActiveCases = $("#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(6)").text();
+
+	var coronainfoOutput = "Total Cases:\n"+njTotalCases+"\n\n"+"New Cases:"+njNewCases+"\n\n"+"Total Deaths:"+njTotalDeaths+"\n\n"+"New Deaths: \n"+njNewDeaths+"\n\n"+"Active Cases: "+njActiveCases;
+	//string to be sent for outputting 
+	var coronainfoOutputHeading="NJ COVID-19 Update\n----------------\n";
+
+	client.messages
+		 .create({
+		body: coronainfoOutputHeading + coronainfoOutput,
+		from: process.env.TWILIO_PHONE_NUMBER,
+		to: process.env.ANT_NUMBER
+		})
+		.then(message => console.log(message.sid));
+	});
+});
+
 app.get("/", function(req, res){
 	
 	res.render("home");
 
-});
-
-schedule.scheduleJob("10 19 * * 0-6", function(){
-
-	axios.get('https://www.worldometers.info/coronavirus/country/us/').then((res)=>{
-
-		const $ = cheerio.load(res.data);
-
-		const njTotalCases = $("#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(2)").text();
-		const njNewCases = $("#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(3)").text();
-		const njTotalDeaths =$("#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(4)").text();
-		const njNewDeaths = $("#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(5)").text();
-		const njActiveCases = $("#usa_table_countries_today > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(6)").text();
-
-		var coronainfoOutput = "Total Cases:\n"+njTotalCases+"\n\n"+"New Cases:"+njNewCases+"\n\n"+"Total Deaths:"+njTotalDeaths+"\n\n"+"New Deaths: \n"+njNewDeaths+"\n\n"+"Active Cases: "+njActiveCases;
-		//string to be sent for outputting 
-		var coronainfoOutputHeading="NJ COVID-19 Update\n----------------\n";
-
-		client.messages
-			 .create({
-			body: coronainfoOutputHeading + coronainfoOutput,
-			from: process.env.TWILIO_PHONE_NUMBER,
-			to: process.env.ANT_NUMBER
-			})
-			.then(message => console.log(message.sid));
-	});
 });
 
 app.listen(port, function(){
